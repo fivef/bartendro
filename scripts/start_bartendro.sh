48ca3b5c845e4f21933593710ef6c373b3f0bbdf
fivef <fivef2@gmail.com>
2016-06-01 21:28:27 +0200

    added uwsgi to requirements, seems to nearly work

--- scripts/start_bartendro.sh
+++ scripts/start_bartendro.sh
@@ -1,4 +1,7 @@
 #/bin/bash
 
-cd /home/pi/dev/bartendro/ui/
+cd ../ui
+
+# TODO: get uwsgi to work for global lock and for production server 
+#uwsgi --socket 192.168.0.26:8000 --protocol=http --enable-threads -w bartendro_server
 sudo python bartendro_server.py -t 192.168.0.26
