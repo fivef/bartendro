10c7e7acdf328ac5f648485599997a8a39c1b4e7
Steffen <s.pfiffner@gmail.com>
2016-06-06 14:21:40 +0200

    user.py minor reformatting

--- ui/bartendro/view/user.py
+++ ui/bartendro/view/user.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 from bartendro import app, db, login_manager
 from bartendro.form.login import LoginForm
-from flask import Flask, request, render_template, flash, redirect, url_for, jsonify
+from flask import request, render_template, redirect, url_for, jsonify
 from flask.ext.login import login_required, login_user, logout_user
 from bartendro.model.user import User
-from flask.ext.permissions.models import UserMixin
-from bartendro.view.root import index
+from bartendro.view.drink.drink import is_ip_allowed_to_pour_drinks
 
 
 @login_manager.user_loader
@@ -21,24 +20,22 @@ def login():
         password = request.form.get("password" or '')
 
         # check if user with these credentials exists
-
         user_object = db.session.query(User).filter(User.name == user)
 
         if user_object.first():
-
             if user_object.first().check_password(password):
-                # import ipdb; ipdb.set_trace()
                 login_user(user_object.first())
-
                 return redirect(request.args.get("next") or "/")
         return render_template("/login",
                                options=app.options,
                                form=form,
-                               fail=1)
+                               fail=1,
+                               allowed_to_pour=is_ip_allowed_to_pour_drinks(request.remote_addr))
     return render_template("/login",
                            options=app.options,
                            form=form,
-                           fail=0)
+                           fail=0,
+                           allowed_to_pour=is_ip_allowed_to_pour_drinks(request.remote_addr))
 
 
 @app.route("/logout")
@@ -47,21 +44,21 @@ def logout():
     logout_user()
     return redirect(url_for("index"))
 
-"""Login user via rfid tag"""
-@app.route('/rfid', methods= ['GET'])                       
+
+@app.route('/rfid', methods=['GET'])
 def rfid():
+    """Login user via rfid tag"""
     print("RFID read")
     tag_id = app.rfid_reader.get_tag()
     user_object = db.session.query(User).filter(User.rfid == tag_id)
-    
+
     if user_object.first():
-        #check if users rfid is empty, if empty ignore
+        # check if users rfid is empty, if empty ignore
         if user_object.first().rfid == "":
             return jsonify(tag_id=tag_id)
         login_user(user_object.first())
         print("User {} logged in".format(user_object.first().name))
-        
-        return jsonify(tag_id=tag_id)
-         #redirect(request.args.get("/shots"))
-    return jsonify(tag_id=tag_id)
 
+        return jsonify(tag_id=tag_id)
+        # TODO correctly redirect to wanted target redirect(url_for("index"))
+    return jsonify(tag_id=tag_id)
